327f3ea9b8cba3cc5900e46477a6da2d18009a20
Christian Klem <christian.klem@nav.no>
2024-02-12 12:55:25 +0100

    Diverse oppdateringer
    Bygg / runtime:
    
    - java 17 -> 21
    - gradle 7.3.3 -> 8.5
    - kotlin 1.7.10 -> 1.9.22
    
    App:
    
    - ktor 2.0.3 -> 2.3.8
    - logback 1.2.11 -> 1.4.14
    - div minor
    
    Gjenstår:
    
    - Bytt til chainguard image?
    - Sjekk for deprications i koden
    - Bedre README

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     java
     application
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.serialization") version "1.7.10"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.22"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 application {
@@ -20,24 +18,25 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
-    val kotestVerstion = "5.4.2"
+    val kotestVerstion = "5.8.0"
     testImplementation("io.kotest:kotest-assertions-core:$kotestVerstion")
 
-    val ktorVersion = "2.0.3"
+    val ktorVersion = "2.3.8"
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
-    implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+
+kotlin {
+    jvmToolchain(21)
 }
 
 tasks.register<JavaExec>("generate-mock-files") {
     classpath = sourceSets.test.get().runtimeClasspath
-    main = "no.nav.arbeidsgiver.mock.enhetsregisteret.GradleTasks"
+    mainClass.set("no.nav.arbeidsgiver.mock.enhetsregisteret.GradleTasks")
 }
