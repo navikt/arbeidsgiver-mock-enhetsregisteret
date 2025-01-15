36d9d1d3491181ac525191bf04a84f19bc383155
nimajimale <nima0512@gmail.com>
2025-01-15 13:33:04 +0100

    Oppdater avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 val kotestVerstion = "5.9.1"
 val ktorVersion = "3.0.1"
-val nettyCodecHttp2Version = "4.1.115.Final"
-val logbackClassicVersion = "1.5.12"
+val nettyCodecHttp2Version = "4.1.116.Final"
+val logbackClassicVersion = "1.5.15"
 val logstashLogbackEncoderVersion = "8.0"
 
 plugins {
     java
     application
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
