a813db100b3d372f3557163c69009cf03c6ebea1
Christian Klem <christian.klem@nav.no>
2026-06-29 14:08:54 +0200

    Trigg nytt bygg

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     constraints {
         implementation("tools.jackson.core:jackson-core") {
             version { require("3.1.1") }
-            because("versjoner <= 3.1.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
+            because("versjoner <= 3.1.0 har sårbarhet inkludert i logstash-logback-encoder:9.0")
         }
     }
 }
